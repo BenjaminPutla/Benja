4eaca838e67e250ec06b4c48b10ccd1cf77475d1
BenjaminPutla <66523890+BenjaminPutla@users.noreply.github.com>
2020-06-06 16:00:44 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,33 +2,33 @@ var miImage = document.querySelector('img');
 
 miImage.onclick = function () {
     var miSrc = miImage.getAttribute('src');
-    if (miSrc == '/images/mexico.png') {
-      miImage.setAttribute('src','/images/brasil.png');
+    if (miSrc == 'images/mexico.png') {
+      miImage.setAttribute('src','images/brasil.png');
   }
     
-else if (miSrc == '/images/brasil.png') {
-      miImage.setAttribute('src','Benja/images/china.png');
+else if (miSrc == 'images/brasil.png') {
+      miImage.setAttribute('src','images/china.png');
   }
-else if (miSrc == '/images/china.png') {
-      miImage.setAttribute('src','/images/argentina.png');
+else if (miSrc == 'images/china.png') {
+      miImage.setAttribute('src','images/argentina.png');
   }  
 else if (miSrc == '/images/argentina.png') {
-      miImage.setAttribute('src','/images/alemania.png');
+      miImage.setAttribute('src','images/alemania.png');
   } 
-else if (miSrc == '/images/alemania.png') {
-      miImage.setAttribute('src','/images/estados unidos.png');
+else if (miSrc == 'images/alemania.png') {
+      miImage.setAttribute('src','images/estados unidos.png');
   }   
 
-else if (miSrc == '/images/estados unidos.png') {
-      miImage.setAttribute('src','/images/uruguay.png');
+else if (miSrc == 'images/estados unidos.png') {
+      miImage.setAttribute('src','images/uruguay.png');
   } 
 
-else if (miSrc == '/images/uruguay.png') {
+else if (miSrc == 'images/uruguay.png') {
       miImage.setAttribute('src','images/costarica.png');
   }  
 
 else{
-miImage.setAttribute('src','/images/mexico.png');
+miImage.setAttribute('src','images/mexico.png');
 }	 
 	 
 }
